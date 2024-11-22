ca653d178f18f677d0dec46047b03439151898c4
NikitaKrylov <idoverchiviiloh@gmail.com>
2024-11-22 13:39:29 +0300

    feat: event datetime formaters

--- app/src/main/java/com/example/eventify/presentation/models/ShortEventItem.kt
+++ app/src/main/java/com/example/eventify/presentation/models/ShortEventItem.kt
@@ -4,7 +4,16 @@ import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.ZoneId
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 import java.util.Locale
+
+
+const val SHORT_DATE_FORMAT = "dd MMM"
+const val SHORT_TIME_FORMAT = "HH:mm"
+
+const val LONG_DATE_FORMAT = "dd MMMM"
+const val LONG_TIME_FORMAT = "HH:mm"
 
 data class ShortEventItem(
     val id: String,
@@ -12,4 +21,23 @@ data class ShortEventItem(
     val description: String,
     val start: Int,
     val end: Int
-)
+){
+    val localDateTimeStart: LocalDateTime
+        get() = LocalDateTime.ofEpochSecond(start.toLong(), 0, ZoneOffset.UTC)
+
+    val localDateTimeEnd: LocalDateTime
+        get() = LocalDateTime.ofEpochSecond(end.toLong(), 0, ZoneOffset.UTC)
+
+}
+
+fun LocalDateTime.time(short: Boolean = true): String {
+    val pattern = if (short) SHORT_TIME_FORMAT else LONG_TIME_FORMAT
+    val formater = DateTimeFormatter.ofPattern(pattern)
+    return this.format(formater)
+}
+
+fun LocalDateTime.date(short: Boolean = true): String {
+    val pattern = if (short) SHORT_DATE_FORMAT else LONG_DATE_FORMAT
+    val formater = DateTimeFormatter.ofPattern(pattern)
+    return this.format(formater)
+}
