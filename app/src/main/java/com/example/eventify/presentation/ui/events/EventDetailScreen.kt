2dde5786650732190341a0330787f89bf02e8bb1
NikitaKrylov <idoverchiviiloh@gmail.com>
2024-12-03 11:47:50 +0300

    fix: make event detail screen scrolable

--- app/src/main/java/com/example/eventify/presentation/ui/events/EventDetailScreen.kt
+++ app/src/main/java/com/example/eventify/presentation/ui/events/EventDetailScreen.kt
@@ -18,8 +18,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -123,6 +125,7 @@ fun EventDetailScreenComponent(
         Column(
             modifier = Modifier
                 .padding(top = paddingValue.calculateTopPadding())
+                .verticalScroll(rememberScrollState())
         ) {
             ImagePager(
                 listOf(
