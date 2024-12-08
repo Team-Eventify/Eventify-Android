bf5a6943bd466e5dcefcc1286750bc0929b4fcf2
NikitaKrylov <idoverchiviiloh@gmail.com>
2024-12-08 21:24:55 +0300

    feat: add components for settings screen

--- app/src/main/java/com/example/eventify/presentation/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/eventify/presentation/ui/profile/ProfileScreen.kt
@@ -1,18 +1,22 @@
 package com.example.eventify.presentation.ui.profile
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.eventify.presentation.models.UserShortInfo
+import com.example.eventify.presentation.ui.profile.components.ActionSettingsItem
+import com.example.eventify.presentation.ui.profile.components.SettingsBlock
+import com.example.eventify.presentation.ui.profile.components.BaseSettingsItem
+import com.example.eventify.presentation.ui.profile.components.NavigationSettingsItem
 import com.example.eventify.presentation.ui.profile.components.UserProfilePanel
 import com.example.eventify.presentation.ui.theme.EventifyTheme
 
@@ -23,6 +27,7 @@ fun ProfileScreen(
 ) {
 
     Column(
+        verticalArrangement = Arrangement.spacedBy(12.dp),
         modifier = Modifier
             .fillMaxSize()
             .padding(10.dp)
@@ -31,11 +36,32 @@ fun ProfileScreen(
             user = state.userInfo!!,
             onClick = actions.navigateToProfileEdit
         )
-        Button(onClick = {
-            actions.onLogOut()
-            // TODO rewrite show logOut dialog
-        }) {
-            Text(text = "Выйти")
+        SettingsBlock {
+            NavigationSettingsItem("Уведомления", onClick = {})
+            HorizontalDivider()
+            NavigationSettingsItem("Помощь и поддержка")
+        }
+
+        SettingsBlock {
+            NavigationSettingsItem("О Прилоожении", onClick = {})
+            HorizontalDivider()
+            NavigationSettingsItem("Оценить")
+        }
+
+        SettingsBlock {
+            ActionSettingsItem(
+                text = "Выйти из аккаунта",
+                onClick = {
+                    actions.onLogOut()
+                }
+            )
+        }
+
+        SettingsBlock {
+            ActionSettingsItem(
+                text = "Удалить аккаунт",
+                onClick = {}
+            )
         }
     }
 }
---
+++ app/src/main/java/com/example/eventify/presentation/ui/profile/components/SettingsBlock.kt
@@ -0,0 +1,148 @@
+package com.example.eventify.presentation.ui.profile.components
+
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.eventify.R
+import com.example.eventify.presentation.ui.shared.AnnotationText
+import com.example.eventify.presentation.ui.theme.EventifyTheme
+
+@Composable
+fun SettingsBlock(
+    modifier: Modifier = Modifier,
+    surfaceContainer: Color = MaterialTheme.colorScheme.surfaceContainer,
+    content: @Composable() (ColumnScope.() -> Unit)
+) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = surfaceContainer
+        ),
+        shape = RoundedCornerShape(12.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier
+        ) {
+            content()
+        }
+    }
+
+}
+
+
+@Composable
+fun BaseSettingsItem(
+    content: @Composable() () -> Unit,
+    leadingIcon: @Composable() (() -> Unit)? = null,
+    trailingIcon: @Composable() (() -> Unit)? = null,
+    onClick: (() -> Unit)? = null
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+                onClick?.invoke()
+            }
+            .padding(12.dp)
+
+    ) {
+        leadingIcon?.invoke()
+        content()
+        trailingIcon?.invoke()
+    }
+}
+
+@Composable
+fun SettingsItemText(
+    text: String
+) {
+    Text(text = text)
+}
+
+@Composable
+fun SupportingSettingsItemText(
+    text: String
+) {
+    AnnotationText(
+        text = text
+    )
+}
+
+@Composable
+fun NavigationSettingsItem(
+    text: String,
+    supportingText: String? = null,
+    onClick: (() -> Unit)? = null
+) {
+    BaseSettingsItem(
+        content = {
+            Column {
+                SettingsItemText(text = text)
+                supportingText?.let {
+                    SupportingSettingsItemText(text = it)
+                }
+            }
+        },
+        onClick = onClick,
+        trailingIcon = {
+            Icon(painter = painterResource(id = R.drawable.ic_arrow_right), contentDescription = null)
+        }
+    )
+}
+
+@Composable
+fun ActionSettingsItem(
+    text: String,
+    onClick: (() -> Unit)?
+) {
+    BaseSettingsItem(
+        content = {
+            SettingsItemText(text)
+        },
+        onClick = onClick
+    )
+}
+
+
+
+
+@Preview(name = "SettingsBlock")
+@Composable
+private fun PreviewSettingsBlock() {
+    EventifyTheme(
+        darkTheme = true
+    ) {
+        Surface {
+            SettingsBlock{
+                NavigationSettingsItem("Navigation item")
+                HorizontalDivider()
+                ActionSettingsItem("Action item", onClick = null)
+                HorizontalDivider()
+                NavigationSettingsItem("Navigation item", supportingText = "With supporting text")
+            }
+        }
+    }
+}
