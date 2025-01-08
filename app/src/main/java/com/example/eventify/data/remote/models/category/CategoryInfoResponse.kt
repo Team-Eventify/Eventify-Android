dc3cdcf2a29f142a59cc28e57be94ac78a3cefe3
NikitaKrylov <idoverchiviiloh@gmail.com>
2025-01-08 06:20:39 +0300

    feat: change signature of api category

--- app/src/main/java/com/example/eventify/data/remote/models/category/CategoryInfoResponse.kt
+++ app/src/main/java/com/example/eventify/data/remote/models/category/CategoryInfoResponse.kt
@@ -4,10 +4,14 @@ import com.example.eventify.domain.models.Category
 
 data class CategoryInfoResponse(
     val id: String,
-    val title: String
+    val title: String,
+    val color: String,
+    val cover: String
 ){
     fun toCategoryInfo() = Category(
         id = id,
-        title = title
+        title = title,
+        color = color,
+        cover = cover
     )
 }
--- app/src/main/java/com/example/eventify/domain/models/Category.kt
+++ app/src/main/java/com/example/eventify/domain/models/Category.kt
@@ -2,5 +2,7 @@ package com.example.eventify.domain.models
 
 data class Category(
     val id: String,
-    val title: String
+    val title: String,
+    val color: String,
+    val cover: String
 )
