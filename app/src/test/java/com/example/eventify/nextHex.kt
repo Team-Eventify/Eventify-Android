340ed433216cc9f1a5e744c30e3b967666752097
NikitaKrylov <idoverchiviiloh@gmail.com>
2025-01-26 23:36:36 +0300

    ref: remove all unit tests

--- app/src/test/java/com/example/eventify/nextHex.kt
+++
@@ -1,9 +0,0 @@
-package com.example.eventify
-
-import kotlin.random.Random
-
-private val rndHex: String get() = Random.nextInt(255).toString(16).uppercase()
-
-fun Random.nextHex(): String {
-    return "#FF$rndHex$rndHex$rndHex"
-}
