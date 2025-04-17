3daa1a3e42ea78104095a1f7aa29331ef4675572
NikitaKrylov <idoverchiviiloh@gmail.com>
2025-04-17 21:48:02 +0300

    feat: transition anims in event feed and profile edit

--- app/src/main/java/com/example/eventify/presentation/ui/account/profileedit/ProfileEditEntry.kt
+++ app/src/main/java/com/example/eventify/presentation/ui/account/profileedit/ProfileEditEntry.kt
@@ -1,6 +1,11 @@
 package com.example.eventify.presentation.ui.account.profileedit
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
 import com.example.eventify.presentation.navigation.BaseDestination
 import com.example.eventify.presentation.navigation.ComposableFeatureEntry
@@ -19,5 +24,22 @@ class ProfileEditEntryImpl @Inject constructor() : ProfileEditEntry {
     override fun Composable(navController: NavHostController) {
         ProfileEditRoute(navController)
     }
+
+    override val enterTransition: (AnimatedContentTransitionScope<NavBackStackEntry>.() -> EnterTransition?)?
+        get() = {
+            slideIntoContainer(
+                AnimatedContentTransitionScope.SlideDirection.Start,
+                tween(300)
+            )
+        }
+
+
+    override val popExitTransition: (AnimatedContentTransitionScope<NavBackStackEntry>.() -> ExitTransition?)?
+        get() = {
+            slideOutOfContainer(
+                AnimatedContentTransitionScope.SlideDirection.End,
+                tween(300)
+            )
+        }
 
 }
--- app/src/main/java/com/example/eventify/presentation/ui/events/eventdetail/EventDetailEntry.kt
+++ app/src/main/java/com/example/eventify/presentation/ui/events/eventdetail/EventDetailEntry.kt
@@ -1,6 +1,17 @@
 package com.example.eventify.presentation.ui.events.eventdetail
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.Ease
+import androidx.compose.animation.core.Easing
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
 import com.example.eventify.presentation.navigation.ARG_EVENT_ID
 import com.example.eventify.presentation.navigation.BaseDestination
@@ -20,5 +31,22 @@ class EventDetailEntryImpl @Inject constructor() : EventDetailEntry {
     override fun Composable(navController: NavHostController) {
         EventDetailRoute(navController)
     }
+
+    override val enterTransition: (AnimatedContentTransitionScope<NavBackStackEntry>.() -> EnterTransition?)?
+        get() = {
+            slideIntoContainer(
+                AnimatedContentTransitionScope.SlideDirection.Start,
+                tween(300)
+            )
+        }
+
+
+    override val popExitTransition: (AnimatedContentTransitionScope<NavBackStackEntry>.() -> ExitTransition?)?
+        get() = {
+            slideOutOfContainer(
+                AnimatedContentTransitionScope.SlideDirection.End,
+                tween(300)
+            )
+        }
 
 }
