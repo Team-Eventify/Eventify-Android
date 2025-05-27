2e45cc62133e40e5b8e584fb167c4e68ce3c06f7
NikitaKrylov <idoverchiviiloh@gmail.com>
2025-05-27 18:58:30 +0300

    fix: test gradient with materials colors

--- feature/eventFeed/impl/src/main/kotlin/feature/eventFeed/impl/components/LoadingEventFeed.kt
+++ feature/eventFeed/impl/src/main/kotlin/feature/eventFeed/impl/components/LoadingEventFeed.kt
@@ -1,9 +1,11 @@
 package feature.eventFeed.impl.components
 
 import android.content.res.Configuration
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -12,7 +14,10 @@ import uikit.EventifyTheme
 
 @Composable
 internal fun LoadingEventFeed() {
-    LazyColumn {
+    LazyColumn(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.background)
+    ) {
         items(5) {
             LoadingEventCard()
             Spacer(Modifier.height(10.dp))
