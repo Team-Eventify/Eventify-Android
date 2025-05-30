e49e2858f6e848491142b09bd84d007cb2140396
NikitaKrylov <idoverchiviiloh@gmail.com>
2025-05-30 22:02:53 +0300

    feat: error and empty state in search

--- feature/search/impl/src/main/kotlin/feature/search/impl/ui/SearchScreen.kt
+++ feature/search/impl/src/main/kotlin/feature/search/impl/ui/SearchScreen.kt
@@ -1,8 +1,9 @@
 package feature.search.impl.ui
 
 import android.content.res.Configuration
-import androidx.compose.foundation.background
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,16 +12,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SegmentedButton
 import androidx.compose.material3.SegmentedButtonDefaults
 import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
+import com.example.eventify.uikit.R
 import feature.search.impl.components.CategoriesSearch
 import feature.search.impl.components.EventsSearch
 import feature.search.impl.components.FailedSearch
@@ -35,63 +42,85 @@ import uikit.components.topBar.SearchTopBar
 import uikit.space10
 import uikit.space32
 import uikit.EventifyTheme
+import uikit.components.screens.ErrorScreen
+import uikit.components.screens.NothingFound
+import uikit.space16
+import uikit.space8
+import com.example.eventify.feature.search.impl.R as SearchR
+import com.example.eventify.uikit.R as UiKitR
+
 
 @Composable
 fun SearchScreen(
     state: SearchUiState,
     listener: SearchListener,
 ) {
-    val dimentions = LocalDimentions.current
-
-    Column(
-        verticalArrangement = Arrangement.spacedBy(space10, Alignment.Top),
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
-            .padding(dimentions.windowPaddings)
-    ) {
-
-        SearchTopBar(
-            value = state.searchText,
-            onValueChanged = listener::onChangeSearchQuery,
-            onTrailingIconClick = listener::cleanSearch,
-            onLeadingIconClick = listener::search,
-            onSearch = listener::search
-        )
-
-        SegmentedTabSwitcher(
-            tabs = SearchMode.entries,
-            onTabClick = listener::changeSearchMode,
-            currentTab = state.searchMode,
-        )
-
-        when (state.searchResult) {
-            is SearchResult.Categories -> {
-                CategoriesSearch(
-                    categories = state.searchResult.items,
-                    onCategoryClick = listener::onCategoryClick,
-                )
-            }
-            is SearchResult.Error -> {
-                FailedSearch(
-                    message = state.searchResult.message,
-                    onRefresh = listener::refresh,
-                )
-            }
-            is SearchResult.Events -> {
-                EventsSearch(
-                    events = state.searchResult.items,
-                    onEventClick = listener::onEventClick,
-                )
-            }
-            SearchResult.Empty -> {
-            }
-            SearchResult.None -> {}
-        }
+    val dims = LocalDimentions.current
+    val searchPlaceholder = when (state.searchMode) {
+        SearchMode.Categories -> stringResource(SearchR.string.search_categories_placeholder)
+        SearchMode.Events -> stringResource(SearchR.string.search_events_placeholder)
     }
 
+    Scaffold(
+        topBar = {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(space10),
+                modifier = Modifier
+                    .padding(horizontal = space16)
+                    .padding(bottom = space8)
+            ) {
+                SearchTopBar(
+                    value = state.searchText,
+                    onValueChanged = listener::onChangeSearchQuery,
+                    onTrailingIconClick = listener::cleanSearch,
+                    placeholder = searchPlaceholder,
+                    onLeadingIconClick = listener::search,
+                    onSearch = listener::search
+                )
+
+                SegmentedTabSwitcher(
+                    tabs = SearchMode.entries,
+                    onTabClick = listener::changeSearchMode,
+                    currentTab = state.searchMode,
+                )
+            }
+        }
+    ) { innerPaddings ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = innerPaddings.calculateTopPadding())
+                .padding(horizontal = space16)
+            ,
+        ) {
+            when (state.searchResult) {
+                is SearchResult.Categories -> {
+                    CategoriesSearch(
+                        categories = state.searchResult.items,
+                        onCategoryClick = listener::onCategoryClick,
+                        modifier = Modifier
 
 
+                    )
+                }
+                is SearchResult.Error -> {
+                    ErrorScreen(
+                        title = stringResource(UiKitR.string.failed_to_load_data)
+                    )
+                }
+                is SearchResult.Events -> {
+                    EventsSearch(
+                        events = state.searchResult.items,
+                        onEventClick = listener::onEventClick,
+                    )
+                }
+                SearchResult.Empty -> {
+                    NothingFound()
+                }
+                SearchResult.None -> {}
+            }
+        }
+    }
 }
 
 
@@ -114,11 +143,12 @@ fun SegmentedTabSwitcher(
                 onClick = {
                     onTabClick(tab)
                 },
+                border = BorderStroke(width = 2.dp, color = MaterialTheme.colorScheme.surfaceContainerHighest),
                 colors = SegmentedButtonDefaults.colors(
-                    activeContainerColor = MaterialTheme.colorScheme.surfaceContainer,
+                    activeContainerColor = MaterialTheme.colorScheme.surfaceContainerHighest,
                     inactiveContainerColor = Color.Transparent,
                     activeBorderColor = Color.Transparent,
-                    inactiveBorderColor = MaterialTheme.colorScheme.surfaceContainer,
+                    inactiveBorderColor = MaterialTheme.colorScheme.surfaceContainerHighest,
                     activeContentColor = MaterialTheme.colorScheme.primary,
                 ),
                 shape = SegmentedButtonDefaults.itemShape(
@@ -140,16 +170,12 @@ fun SegmentedTabSwitcher(
 @Composable
 @Preview(name = "Search", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Preview(name = "Search", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
-private fun SearchScreenPreview() {
+private fun SearchScreenPreview(
+    @PreviewParameter(SearchScreenPreviewProvider::class) state: SearchUiState
+) {
     EventifyTheme  {
         SearchScreen(
-            state = SearchUiState(
-                searchMode = SearchMode.Events,
-                searchResult = SearchResult.Events(
-                    items = emptyList()
-                ),
-                searchText = ""
-            ),
+            state = state,
             listener = object : SearchListener {
                 override fun changeSearchMode(mode: SearchMode) = Unit
                 override fun onEventClick(eventId: EventId) = Unit
@@ -161,5 +187,42 @@ private fun SearchScreenPreview() {
             }
         )
     }
+}
+
+
+class SearchScreenPreviewProvider : PreviewParameterProvider<SearchUiState> {
+    override val values: Sequence<SearchUiState> = sequenceOf(
+        SearchUiState(
+            searchMode = SearchMode.Events,
+            searchResult = SearchResult.Categories(
+                items = emptyList()
+            ),
+            searchText = ""
+        ),
+        SearchUiState(
+            searchMode = SearchMode.Events,
+            searchResult = SearchResult.Events(
+                items = emptyList()
+            ),
+            searchText = ""
+        ),
+        SearchUiState(
+            searchMode = SearchMode.Events,
+            searchResult = SearchResult.Events(
+                items = emptyList()
+            ),
+            searchText = "test text",
+        ),
+        SearchUiState(
+            searchMode = SearchMode.Events,
+            searchResult = SearchResult.Error(),
+            searchText = ""
+        ),
+        SearchUiState(
+            searchMode = SearchMode.Events,
+            searchResult = SearchResult.Empty,
+            searchText = ""
+        )
+    )
 }
 
